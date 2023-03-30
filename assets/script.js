6f492d8af1544ac25bf897da40da58f002ac06b6
hasan12022012 <122780042+hasan12022012@users.noreply.github.com>
2023-03-30 09:00:19 +0400

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -7,22 +7,23 @@ let todo = [];
 
 
 function add() {
-    todo.push(inp.value);
-    inp.value = '';
-    let data = ''
-    for (let item of todo) {
-        data += `<li class="list-group-item">${item}</li>`
 
-        if(!todo.includes(inp.value)){
-            todo.pop();
-        }
-        if((inp.value !='')){
-            todo.pop();
-        }
+
+    if (inp.value == '') {
+        alert('')
     }
-    
-    list.innerHTML = data;
-    console.log(todo);
+    else {
+        todo.push(inp.value);
+        inp.value = '';
+        let data = ''
+        for (let item of todo) {
+            data += `<li class="list-group-item">${item}</li>`
+        }
+
+        list.innerHTML = data;
+        console.log(todo);
+    }
+
 }
 
 btn.addEventListener('click', add)
